cc321a6793ac0fc38d1a78b059186fe2e0827fc5
Comando562 <leo.moya562@gmail.com>
2024-04-05 11:14:54 -0600

    Implementacion Completa

--- permiso-service/src/main/java/mx/com/tcs/permiso/service/PermisoServiceImpl.java
+++ permiso-service/src/main/java/mx/com/tcs/permiso/service/PermisoServiceImpl.java
@@ -7,8 +7,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,16 +37,14 @@ public class PermisoServiceImpl implements  IPermisoService {
      * @return a list of Permiso object.
      */
     private List<PermisoDTO> getAllPermiso() {
-        List<PermisoDTO> permisoDTOList = new ArrayList<>();
+        List<PermisoDTO> permisoDTOList;
 
         List<Permiso> permisoList = (List<Permiso>) repository.findAll();
-        System.out.println("respuesta de permiso list");
         permisoList.forEach(System.out::println);
         permisoDTOList = permisoList
                 .stream()
                 .map(permiso -> modelMapper.map(permiso, PermisoDTO.class))
                 .collect(Collectors.toList());
-        System.out.println("Se ejecuto bien el permiso Service");
 
         return permisoDTOList;
     }
